4573b4387005cf46b3a1821f00c300802e4b9449
deepak <deepak@vakilsearch.com>
2016-04-19 11:13:48 +0530

    changes

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module Sample
         resource '*', :headers => :any, :methods => [:get, :post, :options,:put,:delete]
       end
     end
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
   end
 end
